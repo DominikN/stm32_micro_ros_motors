c293f3d3b1c45e35cc20594e881d3f100228e2df
dominikn <dominik.nwk@gmail.com>
2022-01-06 16:21:33 +0100

    basic runtime stats

--- src/Main.cpp
+++ src/Main.cpp
@@ -54,17 +54,18 @@ void error_loop() {
 
 void subscription_callback(const void *msgin) {
   const std_msgs__msg__String *msg = (const std_msgs__msg__String *)msgin;
-  Serial.printf("[%s]: I heard: [%s]\r\n", NODE_NAME,
-                micro_ros_string_utilities_get_c_str(msg->data));
+  // Serial.printf("[%s]: I heard: [%s]\r\n", NODE_NAME,
+  //               micro_ros_string_utilities_get_c_str(msg->data));
 }
 
 static void rclc_spin_task(void *p);
 static void chatter_publisher_task(void *p);
+static void runtime_stats_task(void *p);
 
 EncMotors Motor1;
 
 void setup() {
-  portBASE_TYPE s1, s2;
+  portBASE_TYPE s1, s2, s3;
 
   // Open serial communications and wait for port to open:
   Serial.setRx(PA10);
@@ -72,19 +73,19 @@ void setup() {
   Serial.begin(115200);
 
   // ================== TESTING MOTORS ==========================
-  int i = 100;
+  // int i = 100;
 
-  Motor1.setPower(35);
+  // Motor1.setPower(35);
 
-  while (1) {
-    // Serial.printf("PWM = %d\r\n", i);
-    // i--;
-    // Motor1.setPower(i);
-    // Motor1.setPower(-30);
-    // delay(1000);
-    // Motor1.setPower(30);
-    // delay(1000);
-  }
+  // while (1) {
+  //   // Serial.printf("PWM = %d\r\n", i);
+  //   // i--;
+  //   // Motor1.setPower(i);
+  //   // Motor1.setPower(-30);
+  //   // delay(1000);
+  //   // Motor1.setPower(30);
+  //   // delay(1000);
+  // }
 
   // =============================================================
 
@@ -135,8 +136,13 @@ void setup() {
                    configMINIMAL_STACK_SIZE + 1000, NULL, tskIDLE_PRIORITY + 1,
                    NULL);
 
+  // create spin task
+  s3 = xTaskCreate(runtime_stats_task, "runtime_stats_task",
+                   configMINIMAL_STACK_SIZE + 2000, NULL, tskIDLE_PRIORITY + 1,
+                   NULL);
+
   // check for creation errors
-  if (s1 != pdPASS || s2 != pdPASS) {
+  if (s1 != pdPASS || s2 != pdPASS || s3 != pdPASS) {
     Serial.println(F("Creation problem"));
     while (1)
       ;
@@ -151,7 +157,7 @@ static void rclc_spin_task(void *p) {
 
   TickType_t xLastWakeTime = xTaskGetTickCount();
   while (1) {
-    RCCHECK(rclc_executor_spin(&executor));
+    RCSOFTCHECK(rclc_executor_spin_some(&executor, 10));
     vTaskDelayUntil(&xLastWakeTime, 10);
   }
 }
@@ -164,13 +170,23 @@ static void chatter_publisher_task(void *p) {
     char buffer[50];
     static int cnt = 0;
     sprintf(buffer, "Hello World: %d, sys_clk: %d", cnt++, xTaskGetTickCount());
-    // Serial.printf("%s\r\n", buffer);
+    // Serial.printf("Publishing: %s\r\n", buffer);
 
     msg.data = micro_ros_string_utilities_set(msg.data, buffer);
 
     RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
     // vTaskDelay(pdMS_TO_TICKS(1000));
-    vTaskDelayUntil(&xLastWakeTime, 1000);
+    vTaskDelayUntil(&xLastWakeTime, 100);
+  }
+}
+
+static void runtime_stats_task(void *p) {
+  char buf[2000];
+  Serial.printf("runtime stats task started");
+  while(1) {
+    vTaskGetRunTimeStats(buf);
+    Serial.printf("\r\n%s\r\n-------------",buf);
+    vTaskDelay(2000);
   }
 }
 
@@ -178,4 +194,23 @@ void loop() {
   // Serial.printf("Error ");
   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
   delay(1000);
+}
+
+
+// =========== TEMP ====================
+
+HardwareTimer stats_tim(TIM5);  // TIM5 - 32 bit
+
+void vConfigureTimerForRunTimeStats( void )
+{ 
+  //   m1_pwm.setPWM(1, M1_PWM, 1000, power);
+    stats_tim.setPrescaleFactor(1680); // Set prescaler to 2564 => timer frequency = 168MHz/1680 = 100000 Hz (from prediv'd by 1 clocksource of 168 MHz)
+    stats_tim.setOverflow(0xffffffff); // Set overflow to 32761 => timer frequency = 65522 Hz / 32761 = 2 Hz
+    stats_tim.refresh(); // Make register changes take effect
+    stats_tim.resume(); // Start
+}
+
+uint32_t vGetTimerValueForRunTimeStats( void )
+{
+  return stats_tim.getCount();
 }
